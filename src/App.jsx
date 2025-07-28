bbe32ba458efdea8c8925c900bb95ca313d57a78
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-28 22:29:04 +0200

    Added the questions data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
-
+import { useState } from "react"
 
 function App() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answered, setAnswered] = useState(false);
+
+  function handleNextQ(){
+    setCurrentQuestion(currentQuestion + 1)
+  }
+
+  const questions = [
+    {
+      questionText: 'Who is the president of SA?',
+      answerOptions: [
+        {answerText: 'Donald Trump', isCorrect: false},
+        {answerText: 'Jacob Zuma', isCorrect: false},
+        {answerText: 'Cyril Rhamaphosa', isCorrect: true},
+        {answerText: 'Julius Malema', isCorrect: false},
+      ]
+    },
+    {
+      questionText: 'Which province is Malamulele located in?',
+      answerOptions: [
+        {answerText: 'Gauteng', isCorrect: false},
+        {answerText: 'Mpumalanga', isCorrect: false},
+        {answerText: 'Wetsern Cape', isCorrect: false},
+        {answerText: 'Limpopo', isCorrect: true},
+      ]
+    },
+  ]
+
 
   return (
     <>
