c3b06d231810951475ea0300a22b6e30940fb83e
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-29 19:19:01 +0200

    Added the first part of the 2023 P1 LS

---
+++ LifeScience/2023P1.js
@@ -0,0 +1,112 @@
+const questions = [
+  {
+    questionText: 'Which ONE of the following parts controls the amount of light entering the eye by influencing the size of the pupil?',
+    answerOptions: [
+        { answerText: 'Cornea', isCorrect: false },
+        { answerText: 'Retina', isCorrect: false },
+        { answerText: 'Iris', isCorrect: true },
+        { answerText: 'Sclera', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'The iris controls the amount of light entering the eye by adjusting the size of the pupil. It contracts or dilates in response to light intensity.',
+    },
+    {
+    questionText: 'The function of the umbilical vein is to transport …',
+    answerOptions: [
+        { answerText: 'Carbon dioxide from the foetus to the mother.', isCorrect: false },
+        { answerText: 'Nutrients from the mother to the foetus.', isCorrect: true },
+        { answerText: 'Carbon dioxide from the mother to the foetus.', isCorrect: false },
+        { answerText: 'Nutrients from the foetus to the mother.', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'The umbilical vein transports oxygenated blood and nutrients from the mother to the foetus, ensuring proper development.',
+    },
+    {
+    questionText: 'Which ONE of the following represents gland X and hormone Y in homeostatic control of blood glucose?',
+    answerOptions: [
+        { answerText: 'Pancreas, Insulin', isCorrect: false },
+        { answerText: 'Pituitary, Insulin', isCorrect: false },
+        { answerText: 'Pancreas, Glucagon', isCorrect: true },
+        { answerText: 'Pituitary, Glucagon', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'The pancreas detects changes in blood glucose levels and releases glucagon when glucose levels are low. Glucagon stimulates the liver to release stored glucose.',
+    },
+    {
+    questionText: 'Which ONE of the following is CORRECT regarding the homeostatic control of carbon dioxide concentration in the blood?',
+    answerOptions: [
+        { answerText: 'High oxygen levels is the stimulus.', isCorrect: false },
+        { answerText: 'Breathing muscles are the effectors.', isCorrect: true },
+        { answerText: 'The lungs have receptors.', isCorrect: false },
+        { answerText: 'The process is controlled by the cerebrum.', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'Breathing muscles, such as the diaphragm and intercostal muscles, act as effectors in controlling carbon dioxide levels by adjusting the breathing rate.',
+    },
+    {
+    questionText: 'The plant hormones that can be used to kill broad-leaved weeds are …',
+    answerOptions: [
+        { answerText: 'Auxins only.', isCorrect: true },
+        { answerText: 'Abscisic acid and gibberellins.', isCorrect: false },
+        { answerText: 'Abscisic acid and auxins.', isCorrect: false },
+        { answerText: 'Abscisic acid only.', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'Auxins are commonly used in herbicides to selectively kill broad-leaved weeds while sparing grasses and cereals.',
+    },
+    {
+    questionText: 'A girl looking at a car moving away from her is able to focus on the letters on the number plate. Which ONE of the following changes occurred in her eyes?',
+    answerOptions: [
+        { answerText: 'The lens became more convex.', isCorrect: false },
+        { answerText: 'Light rays were refracted more.', isCorrect: false },
+        { answerText: 'The suspensory ligaments slackened.', isCorrect: false },
+        { answerText: 'The ciliary muscles relaxed.', isCorrect: true },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'When viewing distant objects, the ciliary muscles relax, causing the lens to flatten and reduce refraction.',
+    },
+    {
+    questionText: 'One of the characteristics of a sperm that causes it to move faster is the …',
+    answerOptions: [
+        { answerText: 'Oval-shaped head.', isCorrect: false },
+        { answerText: 'Presence of enzymes in the acrosome.', isCorrect: true },
+        { answerText: 'Haploid nucleus.', isCorrect: false },
+        { answerText: 'Absence of a middle piece.', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'Enzymes in the acrosome play a critical role in penetrating the egg, and their presence is linked to enhanced motility in sperm cells.',
+    },
+    {
+    questionText: 'In a person suffering from long-sightedness, …',
+    answerOptions: [
+        { answerText: 'Light rays are refracted more by the lens.', isCorrect: false },
+        { answerText: 'Distant objects will appear blurred.', isCorrect: false },
+        { answerText: 'The eyeball is longer than normal.', isCorrect: false },
+        { answerText: 'Light rays fall behind the retina.', isCorrect: true },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'Long-sightedness (hyperopia) occurs when light rays focus behind the retina, making it hard to see nearby objects clearly.',
+    },
+    {
+    questionText: 'Which ONE of the following statements is a conclusion that can be made from the results shown in the graph?',
+    answerOptions: [
+        { answerText: 'As the size of the pupil decreases, the distance from the light source increases.', isCorrect: false },
+        { answerText: 'As the distance from the light source decreases, the size of the pupil increases.', isCorrect: false },
+        { answerText: 'As the distance from the light source increases, the size of the pupil increases.', isCorrect: true },
+        { answerText: 'As the size of the pupil increases, the distance from the light source increases.', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'As light intensity decreases with distance, the pupil enlarges to allow more light in. This explains the positive correlation observed on the graph.',
+    },
+    {
+    questionText: 'Which ONE of the following is a combination of events influenced by LH (luteinising hormone)?',
+    answerOptions: [
+        { answerText: '(ii) and (iii) only', isCorrect: true },
+        { answerText: '(i), (ii), (iii), (iv) and (v)', isCorrect: false },
+        { answerText: '(ii), (iii), (iv) and (v) only', isCorrect: false },
+        { answerText: '(iii) only', isCorrect: false },
+    ],
+    image: 'data:image/png;base64,...',
+    explanation: 'LH triggers ovulation and the development of the corpus luteum, making (ii) and (iii) the correct combination.',
+    } 
+]
