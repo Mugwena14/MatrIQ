384ccf511457f833a5bc880eebbd1580e0089e8f
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-07-30 14:26:33 +0200

    Added data of the Geo P1 2024

---
+++ Geography/2024P1.js
@@ -0,0 +1,91 @@
+const questions = [
+    {
+    questionText: 'Refer to sketches A and B below. Various options are provided as possible answers to the following questions based on the influence of anticyclones in South Africa. Choose the answer and write only the letter (A-D) next to the question numbers (1.1.1 to 1.1.8) in the ANSWER BOOK, e.g. 1.1.9 D.',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: true },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Sketch A shows typical winter conditions.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'The low-pressure system in sketch B forms due to ... over the land.',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: true },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'The low-pressure system in sketch B forms due to cold dense air over the land.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: '... is the concept used to describe the elongation of the isobars associated with the South Atlantic anticyclone in sketch B.',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: true },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Riding is the concept used to describe the elongation of the isobars associated with the South Atlantic anticyclone in sketch B.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'The anticyclones in sketch A and B change their positions seasonally because of the ...',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: true },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'The anticyclones in sketch A and B change their positions seasonally because of the shifting of the ITCZ.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'The South Indian anticyclone in sketch B is likely to feed more moisture over the east coast in comparison with sketch A because it ...',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: true },
+        { answerText: 'C', isCorrect: false },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Sketch B shows a larger fetch, meaning the South Indian anticyclone brings more moisture over the east coast due to increased oceanic influence.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'The low-pressure system in sketch A will move in a ... direction.',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: true },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Low-pressure systems in South Africa typically move eastward, especially along the coast.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'Berg winds are most likely to develop in sketch A because of the presence of the ... and ...',
+    answerOptions: [
+        { answerText: 'A', isCorrect: true },
+        { answerText: 'B', isCorrect: false },
+        { answerText: 'C', isCorrect: false },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Berg winds are driven by the Kalahari anticyclone and coastal lows, both of which are present in sketch A.',
+    image: 'data:image/png;base64,...'
+},
+{
+    questionText: 'The inversion layer that forms over the escarpment in sketch B will favour the formation of ... and ...',
+    answerOptions: [
+        { answerText: 'A', isCorrect: false },
+        { answerText: 'B', isCorrect: true },
+        { answerText: 'C', isCorrect: false },
+        { answerText: 'D', isCorrect: false },
+    ],
+    explanation: 'Inversion layers trap cold air and moisture, creating ideal conditions for frost and fog formation.',
+    image: 'data:image/png;base64,...'
+}
+
+]
