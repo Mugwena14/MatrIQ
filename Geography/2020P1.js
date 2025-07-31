c7cddb3877197e89d66f71afe13eac440917af1d
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-01 00:22:29 +0200

    Added the images of the Geo P1

--- Geography/2020P1.js
+++ Geography/2020P1.js
@@ -8,7 +8,7 @@ const questions = [
         { answerText: 'Valley breeze', isCorrect: false },
     ],
     explanation: 'Anabatic winds are caused by warm air rising along slopes during daytime heating.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'Air movement that occurs at the night',
@@ -19,7 +19,7 @@ const questions = [
         { answerText: 'Convective lift', isCorrect: false },
     ],
     explanation: 'Katabatic winds occur at night when cool, dense air flows downslope due to gravity.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'Air movement that originates due to the rate of insolation',
@@ -30,7 +30,7 @@ const questions = [
         { answerText: 'Shadow zone current', isCorrect: false },
     ],
     explanation: 'Valley breezes form as a result of solar heating that causes air to rise from valley floors.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'Air movement that mostly reduces air pollution at the bottom of the valley',
@@ -41,7 +41,7 @@ const questions = [
         { answerText: 'Frost-forming wind', isCorrect: false },
     ],
     explanation: 'Anabatic winds flow downwards, lifting polluted air upwards.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'Air movement associated with dense, heavy air',
@@ -52,7 +52,7 @@ const questions = [
         { answerText: 'Thermal uplift', isCorrect: false },
     ],
     explanation: 'Cold air drainage involves dense air flowing downslope, usually at night.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'Air movement associated with the formation of frost on the valley floor',
@@ -63,7 +63,7 @@ const questions = [
         { answerText: 'Valley breeze', isCorrect: false },
     ],
     explanation: 'Katabatic winds bring cold, dense air into valleys, often leading to frost formation.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 },
 {
     questionText: 'The direction of air movement determined by gravitational forces',
@@ -74,7 +74,7 @@ const questions = [
         { answerText: 'Thermal convection', isCorrect: false },
     ],
     explanation: 'Gravity flow describes air movement caused purely by the pull of gravity, often downward.',
-    image: 'data:image/png;base64,...'
+    image: undefined,
 }
 
 
