d1a1a6bcadb240da63e8e1f3a6128eda886437d7
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-02 10:29:36 +0200

    Added react layouts

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <p>Langavi Will Be BIG</p>
+            </header>
+        </div>
+    )
+}
+
+export default Header
