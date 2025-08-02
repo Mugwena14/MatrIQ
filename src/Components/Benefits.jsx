329942265a3a696c6d6ee1b70eae04df1a8bb8c7
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-02 14:07:47 +0200

    Added the benefits of using MatrIQ

---
+++ src/Components/Benefits.jsx
@@ -0,0 +1,39 @@
+import styles from './Benefits.module.css'
+
+const Benefits = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.cont}>
+                <div className={styles.image}>
+                    <img src="" alt="" />
+                </div>
+                <h4>IMMEDIATE FEEDBACK</h4>
+                <p>Get instant results after each answer
+                    helping you learn faster and retain more.
+                </p>
+            </div>
+            <div className={styles.cont}>
+                <div className={styles.image}>
+                    <img src="" alt="" />
+                </div>
+                <h4>GENERATE QUIZ</h4>
+                <p>
+                    Generate your own quiz, filtering by topics &
+                    difficulty. Perfect for focused revision.
+                </p>
+            </div>
+            <div className={styles.cont}>
+                <div className={styles.image}>
+                    <img src="" alt="" />
+                </div>
+                <h4>DARK MODE</h4>
+                <p>
+                    Study longer with less strain. MatrIQ's dark Mode
+                    is easy on the eyes and battery-friendly.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Benefits
