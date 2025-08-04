cef1d8162faf523ff127c2d183b5cb9349313841
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-04 16:02:04 +0200

    Added LS Page and its HTML

---
+++ src/Components/LS.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './LS.module.css'
+
+const LS = () => {
+    return (
+        <div className={styles.LS}>
+            <div className={styles.box}>
+                <h1>Geography</h1>
+                <div className={styles.paper}>
+                    <p><button>Life Sciences Paper 1</button></p>
+                    <p><button>Life Sciences Paper 2</button></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LS
