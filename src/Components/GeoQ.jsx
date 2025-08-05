d18372024d012d88f36ea86c89df49c39fbc043b
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-05 12:43:31 +0200

    Geo Q more responsiveness

--- src/Components/GeoQ.jsx
+++ src/Components/GeoQ.jsx
@@ -6,7 +6,7 @@ const GeoQ = () => {
     return (
         <div className={styles.geoQ}>
             <div className={styles.boxQ}>
-                <h1>Geography</h1>
+                <h1>Geography Paper 1</h1>
                 <div className={styles.paperQ}>
                     <p><Link to='/'><button className={styles.generateQ}>Generate My Quiz</button></Link></p>
                     <p><Link to='/'><button>Geography NSC P1 Nov 2024</button></Link></p>
