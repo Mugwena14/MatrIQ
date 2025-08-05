264479133ed0ca3841e247ad6c508d78f5c0eecf
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-05 12:02:24 +0200

    Added Geo Questions to route

---
+++ src/Components/GeoQ.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GeoQ = () => {
+    return (
+        <div>GeoQ</div>
+    )
+}
+
+export default GeoQ
---
+++ src/Pages/GeoQPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GeoQ from '../Components/GeoQ'
+
+const GeoQPage = () => {
+    return (
+        <GeoQ/>
+    )
+}
+
+export default GeoQPage
