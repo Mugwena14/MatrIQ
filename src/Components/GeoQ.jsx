e8bda2676fed5e015cc0c733c746760161b4420c
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-05 12:08:17 +0200

    Added the Geo Q HTML

--- src/Components/GeoQ.jsx
+++ src/Components/GeoQ.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const GeoQ = () => {
     return (
-        <div>GeoQ</div>
+        <div className={styles.geoQ}>
+            <div className={styles.boxQ}>
+                <h1>Geography</h1>
+                <div className={styles.paperQ}>
+                    <Link to='/'><button>Generate My Quiz</button></Link>
+                    <Link to='/'><button>Geography NSC P1 Nov 2024</button></Link>
+                    <Link to='/'><button>Geography NSC P1 Nov 2024</button></Link>
+                    <Link to='/'><button>Geography NSC P1 Nov 2024</button></Link>
+                    <Link to='/'><button>Geography NSC P1 Nov 2024</button></Link>
+                    <Link to='/'><button>Geography NSC P1 Nov 2024</button></Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
