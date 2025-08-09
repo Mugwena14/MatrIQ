71fb2deaee1937d343f1c6877cb9baa0d4b44ccc
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-09 03:32:42 +0200

    Added the Geo P2 id Routes

--- src/Components/GeoQ2.jsx
+++ src/Components/GeoQ2.jsx
@@ -9,10 +9,10 @@ const GeoQ2 = () => {
                 <h1>Geography Paper 2</h1>
                 <div className={styles.paperQ}>
                     <p><Link to='/'><button className={styles.generateQ}>Generate My Quiz</button></Link></p>
-                    <p><Link to='/'><button>Geography NSC P2 Nov 2024</button></Link></p>
-                    <p><Link to='/'><button>Geography NSC P2 Nov 2023</button></Link></p>
-                    <p><Link to='/'><button>Geography NSC P2 Nov 2022</button></Link></p>
-                    <p><Link to='/'><button>Geography NSC P2 Nov 2021</button></Link></p>
+                    <p><Link to='/Quiz/9'><button>Geography NSC P2 Nov 2024</button></Link></p>
+                    <p><Link to='/Quiz/8'><button>Geography NSC P2 Nov 2023</button></Link></p>
+                    <p><Link to='/Quiz/7'><button>Geography NSC P2 Nov 2022</button></Link></p>
+                    <p><Link to='/Quiz/6'><button>Geography NSC P2 Nov 2021</button></Link></p>
                     <p><Link to='/'><button>Geography NSC P2 Nov 2020</button></Link></p>
                 </div>
             </div>
