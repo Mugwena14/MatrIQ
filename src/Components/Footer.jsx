9aec22c3675f4ac7a1df9c69fec7aac122a3ab1f
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-16 23:18:23 +0200

    Added light mode for the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,74 +7,82 @@ import {
     FaXTwitter
 } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
+import { useTheme } from '../Contexts/ThemeContext'
 
 const date = new Date().getFullYear();
-
 const Footer = () => {
+
+    const { theme } = useTheme();
+    
+    // Picked the right style based on theme
+    const footerTheme = theme === "light" ? styles.footerLight : styles.footerDark;
+    
     return (
-        <footer>
-            <div className={styles.footer}>
-                <div className={styles.cont}>
-                    <div className={styles.leftLogo}>
-                        <h2 className={styles.logo}>
-                        <span  className={styles.logoL}>
-                            M
-                        </span>
-                        IQ
-                        </h2>
+        <section className={footerTheme}>
+            <footer>
+                <div className={styles.footer}>
+                    <div className={styles.cont}>
+                        <div className={styles.leftLogo}>
+                            <h2 className={styles.logo}>
+                            <span  className={styles.logoL}>
+                                M
+                            </span>
+                            IQ
+                            </h2>
+                        </div>
+                            <p> Your journey only starts here <br />
+                            Keep revising, Keep Growing <br />
+                            "The hungry don't rest" and neither does<br />
+                            MatrIQ. Big features on the way. <br />
+                            HAPPY PREP.
+                            </p>
+                        <div className={styles.icons}>
+                            <Link className={styles.icon1} to='https://github.com/Mugwena14'><FaSquareGithub /></Link>
+                            <Link className={styles.icon2} to='https://linkedin.com/in/langavi-makhubele-clyde'><FaLinkedin /></Link>
+                            <Link className={styles.icon3} to='https://x.com/MugwenaDev'><FaXTwitter /></Link>
+                            <Link className={styles.icon4} to='https://youtube.com/@MugwenaDev'><FaYoutube /></Link>
+                        </div>
+                        
                     </div>
-                        <p> Your journey only starts here <br />
-                        Keep revising, Keep Growing <br />
-                        "The hungry don't rest" and neither does<br />
-                        MatrIQ. Big features on the way. <br />
-                        HAPPY PREP.
-                        </p>
-                    <div className={styles.icons}>
-                        <Link className={styles.icon1} to='https://github.com/Mugwena14'><FaSquareGithub /></Link>
-                        <Link className={styles.icon2} to='https://linkedin.com/in/langavi-makhubele-clyde'><FaLinkedin /></Link>
-                        <Link className={styles.icon3} to='https://x.com/MugwenaDev'><FaXTwitter /></Link>
-                        <Link className={styles.icon4} to='https://youtube.com/@MugwenaDev'><FaYoutube /></Link>
+                    <div className={styles.cont}>
+                        <div className={styles.top}>
+                            <h3>CONTACT</h3>
+                        </div>
+                        <div className={styles.info}>
+                            <p><Link className={styles.links} to='https://github.com/Mugwena14'>GitHub</Link></p>
+                            <p><Link className={styles.links} to='https://linkedin.com/in/langavi-makhubele-clyde'>LinkedIn</Link></p>
+                            <p><Link className={styles.links} to='maitlto:mlangaviclyde@gmail.com'>Email</Link></p>
+                            <p><Link className={styles.links} to='tel:0685021117'>Phone</Link></p>
+                        </div>
                     </div>
-                    
+                    <div className={styles.cont}>
+                        <div className={styles.top}>
+                            <h3>HELP</h3>
+                        </div>
+                        <div className={styles.info}>
+                            <p><Link className={styles.links} to=''>How to use</Link></p>
+                            <p><Link className={styles.links} to='/Terms'>Terms & Conditions</Link></p>
+                            <p><Link className={styles.links} to='/Privacy'>Policy & Privacy</Link></p>
+                            <p>All rights Reserved</p>
+                        </div>
+                    </div>
+                    <div className={styles.cont}>
+                        <div className={styles.top}>
+                            <h3>RESOURCES</h3>
+                        </div>
+                        <div className={styles.info}>
+                            <p>HTML / CSS</p>
+                            <p>React</p>
+                            <p>FireBase</p>
+                            <p>Git</p>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.cont}>
-                    <div className={styles.top}>
-                        <h3>CONTACT</h3>
+                    <div className={styles.copyright}>
+                        <p>&copy; {date} Copyright: MatrIQ</p>
                     </div>
-                    <div className={styles.info}>
-                        <p><Link className={styles.links} to='https://github.com/Mugwena14'>GitHub</Link></p>
-                        <p><Link className={styles.links} to='https://linkedin.com/in/langavi-makhubele-clyde'>LinkedIn</Link></p>
-                        <p><Link className={styles.links} to='maitlto:mlangaviclyde@gmail.com'>Email</Link></p>
-                        <p><Link className={styles.links} to='tel:0685021117'>Phone</Link></p>
-                    </div>
-                </div>
-                <div className={styles.cont}>
-                    <div className={styles.top}>
-                        <h3>HELP</h3>
-                    </div>
-                    <div className={styles.info}>
-                        <p><Link className={styles.links} to=''>How to use</Link></p>
-                        <p><Link className={styles.links} to='/Terms'>Terms & Conditions</Link></p>
-                        <p><Link className={styles.links} to='/Privacy'>Policy & Privacy</Link></p>
-                        <p>All rights Reserved</p>
-                    </div>
-                </div>
-                <div className={styles.cont}>
-                    <div className={styles.top}>
-                        <h3>RESOURCES</h3>
-                    </div>
-                    <div className={styles.info}>
-                        <p>HTML / CSS</p>
-                        <p>React</p>
-                        <p>FireBase</p>
-                        <p>Git</p>
-                    </div>
-                </div>
-            </div>
-                <div className={styles.copyright}>
-                    <p>&copy; {date} Copyright: MatrIQ</p>
-                </div>
-        </footer>
+            </footer>
+        </section>
     )
 }
 
