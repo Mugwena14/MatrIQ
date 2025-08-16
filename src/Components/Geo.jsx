6fbc09eb873b0573a413d41cd4ee55a686feb5ba
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-17 01:44:07 +0200

    Added both the front pages of Geo and Ls light mode

--- src/Components/Geo.jsx
+++ src/Components/Geo.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import styles from './Geo.module.css'
 import { Link } from 'react-router-dom'
+import { useTheme } from '../Contexts/ThemeContext'
 
 const Geo = () => {
+
+    const { theme } = useTheme();
+    
+    // Picked the right style based on theme
+    const geoTheme = theme === "light" ? styles.geoLight : styles.geoDark;
+
     return (
-        <div className={styles.geo}>
-            <div className={styles.box}>
-                <h1>Geography</h1>
-                <div className={styles.paper}>
-                    <Link to='/GeoQ'><button>Geography Paper 1</button></Link>
-                    <p><Link to='/GeoQ2'>
-                        <button>Geography Paper 2</button>
-                        </Link>
-                    </p>
+        <div className={geoTheme}>
+            <div className={styles.geo}>
+                <div className={styles.box}>
+                    <h1>Geography</h1>
+                    <div className={styles.paper}>
+                        <Link to='/GeoQ'><button>Geography Paper 1</button></Link>
+                        <p><Link to='/GeoQ2'>
+                            <button>Geography Paper 2</button>
+                            </Link>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,3 +29,8 @@ const Geo = () => {
 }
 
 export default Geo
+
+
+
+
+        
--- src/Components/LS.jsx
+++ src/Components/LS.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import styles from './LS.module.css'
 import { Link } from 'react-router-dom'
+import { useTheme } from '../Contexts/ThemeContext'
+
 
 const LS = () => {
+
+    const { theme } = useTheme();
+    
+    // Picked the right style based on theme
+    const lsTheme = theme === "light" ? styles.lsLight : styles.lsDark;
+
     return (
-        <div className={styles.LS}>
-            <div className={styles.box}>
-                <h1>Life Sciences</h1>
-                <div className={styles.paper}>
-                    <p>
-                        <Link to='/LSQ'><button>Life Sciences Paper 1</button></Link>
-                    </p>
-                    <p>
-                        <Link to='/LSQ2'><button>Life Sciences Paper 2</button></Link>
-                    </p>
+        <div className={lsTheme}>
+            <div className={styles.LS}>
+                <div className={styles.box}>
+                    <h1>Life Sciences</h1>
+                    <div className={styles.paper}>
+                        <p>
+                            <Link to='/LSQ'><button>Life Sciences Paper 1</button></Link>
+                        </p>
+                        <p>
+                            <Link to='/LSQ2'><button>Life Sciences Paper 2</button></Link>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,3 +31,7 @@ const LS = () => {
 }
 
 export default LS
+
+
+
+    
